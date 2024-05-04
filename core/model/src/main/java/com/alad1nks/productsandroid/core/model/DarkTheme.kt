cb4bba5a2b15b4750c761f52bd22c66efb24751b
alad1nks <vasya.sidr777@gmail.com>
2024-05-04 22:58:26 +0300

    feat core: add model module

---
+++ core/model/src/main/java/com/alad1nks/productsandroid/core/model/DarkTheme.kt
@@ -0,0 +1,7 @@
+package com.alad1nks.productsandroid.core.model
+
+enum class DarkTheme {
+    SYSTEM,
+    LIGHT,
+    DARK
+}
---
+++ core/model/src/main/java/com/alad1nks/productsandroid/core/model/Product.kt
@@ -0,0 +1,15 @@
+package com.alad1nks.productsandroid.core.model
+
+data class Product(
+    val id: Long,
+    val title: String,
+    val description: String,
+    val price: Int,
+    val discountPercentage: Float,
+    val rating: Float,
+    val stock: Int,
+    val brand: String,
+    val category: String,
+    val thumbnail: String,
+    val images: List<String>
+)
